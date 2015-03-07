74d46f6baa5bab863e1bacc2c7175b9dc94a29f0
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-03-07 17:05:49 +0900

    Add comment

--- e2e/add.spec.js
+++ e2e/add.spec.js
@@ -2,7 +2,7 @@ describe('豆追加画面', ()=> {
   var pageAdd = require('./components/add.po.js');
   var pageList = require('./components/list.po.js');
 
-  describe('表示', ()=> {
+  describe('表示の振る舞い', ()=> {
     beforeEach(()=> { pageAdd.openThisPage(); });
 
     it('確定ボタン押せる', ()=> {
@@ -26,8 +26,12 @@ describe('豆追加画面', ()=> {
     });
   });
 
-  describe('遷移', ()=> {
-    it('リスト画面から新規登録画面に遷移できること', ()=> {
+  describe('遷移の振る舞い', ()=> {
+    // ページの表示完了確認には、遷移先のページの要素の表示状況を使うことになる。
+    // そのため、遷移のテストは遷移先のページ構造を知る必要がある。テスト対象の
+    // 変更が対応するテストに影響することが望ましいため、遷移先のページに遷移に
+    // 関するテストを記述する。
+    it('リスト画面から豆追加画面へ遷移できること', ()=> {
       pageList.openAddPage();
 
       expect(pageAdd.submitBtn.isPresent()).toBe(true);
